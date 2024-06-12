90b994c56dfddbc6d8c4ad7e52b78f0cc2aedae0
Amir <Amirtzi2072@gmail.com>
2024-06-12 12:56:52 +0300

    Add bottom menu

--- app/src/main/java/com/example/comftyaccess/MainActivity.kt
+++ app/src/main/java/com/example/comftyaccess/MainActivity.kt
@@ -8,12 +8,17 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.recyclerview.widget.RecyclerView.SmoothScroller.Action
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var action: Action
+    private var email:String? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,14 +36,36 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host) as? NavHostFragment
-
-
-        navController = navHostFragment?.navController ?: throw RuntimeException("NavController not found")
+       val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host) as? NavHostFragment
+        navController =
+            navHostFragment?.navController ?: throw RuntimeException("NavController not found")
         setupActionBarWithNavController(navController)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_profile -> {
+                    val bundle = Bundle()
+                    bundle.putString("email", email)
+                    navController.navigate(R.id.action_global_myProfileFragment, bundle)
+                    true
+                }
+                R.id.nav_home -> {
+                    navController.navigate(R.id.action_global_allReviewsFragment)
+                    true
+                }
+                 R.id.nav_filter -> {
+                    navController.navigate(R.id.action_global_filterFragment)
+                    true
+                }
 
+                else -> false
+            }
+
+
+        }
+            
     }
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
