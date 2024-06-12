c821f2815f3d6473019269d3164b2f2f70743e2d
nivperetz2706 <1nivperetz@gmail.com>
2024-06-12 12:20:08 +0300

    main activity commit

--- app/src/main/java/com/example/comftyaccess/MainActivity.kt
+++ app/src/main/java/com/example/comftyaccess/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.comftyaccess
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.mainToolbar)
+        setSupportActionBar(toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.connect_nav_host) as? NavHostFragment
+        navController = navHostFragment?.navController ?: throw RuntimeException("NavController not found")
+        setupActionBarWithNavController(navController)
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/comftyaccess/SignInFragment.kt
+++ app/src/main/java/com/example/comftyaccess/SignInFragment.kt
@@ -1,14 +1,16 @@
 package com.example.comftyaccess
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
-import androidx.navigation.Navigation.findNavController
-import com.example.comftyaccess.databinding.FragmentSignInBinding
 import androidx.navigation.fragment.findNavController
+import com.example.comftyaccess.databinding.FragmentSignInBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,10 +31,6 @@ class SignInFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -46,6 +44,16 @@ class SignInFragment : Fragment() {
             val action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
             findNavController().navigate(action)
         }
+        binding.signInBtn.setOnClickListener { view2 ->
+            val i = Intent(activity, MainActivity::class.java)
+            val email:String = binding.emailEditText.toString()
+            val pass:String = binding.passEditText.toString()
+
+            startActivity(i)
+        }
         return binding.root
+        override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.title = "Sign in"
     }
 }
